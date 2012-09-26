496421fcf7f934ccef36555c3a10d7a0e97f6e90
Bhushan <bhushan@weboniselab.com>
2012-09-26 14:48:53 +0530

    changes in feedback model

--- app/models/user_feedback.rb
+++ app/models/user_feedback.rb
@@ -1,6 +1,14 @@
 class UserFeedback < ActiveRecord::Base
-  attr_accessible :feedback, :from_user_id, :to_user_id
+  attr_accessible :feedback, :from_id, :to_id
 
   belongs_to :from, :class_name => User
   belongs_to :to, :class_name => User
+
+  before_save :check_user
+
+  def check_user
+    if User.find(self.from_id).role == 'admin' || User.find(self.to_id).role == 'admin'
+      return false;
+    end
+  end
 end
