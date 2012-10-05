630bbba4b6a9f70f6cfa622bbf5015d26b77714a
simanchala pradhan <simanchala@weboniselab.com>
2012-10-05 19:26:34 +0530

    changes in cron schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 #set :output, File.join(Whenever.path, "log", "cron.log")
-every :day, :at => '6:36 pm' do
+every :day, :at => '6:00 pm' do
   runner "UserFeedback.alert_mail", :environment => "development"
 end
 #every 5.minutes do
