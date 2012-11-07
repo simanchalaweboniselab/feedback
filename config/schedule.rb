4e3d3bb4139abae26cb3793cc457ad498727e378
simanchala pradhan <simanchala@weboniselab.com>
2012-11-07 12:24:02 +0530

    changes in schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 #set :output, File.join(Whenever.path, "log", "cron.log")
 every :day, :at => '6:30 pm' do
-  runner "UserFeedback.alert_mail", :environment => "development"
+  runner "UserFeedback.alert_mail", :environment => "production"
 end
 #every 2.minutes do
 #  runner "UserFeedback.alert_mail", :environment => "development"
